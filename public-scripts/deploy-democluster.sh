2bcb5edc7dce1ad516c5ac0ada7b42ea1333b3e7
jamesbeedy <jamesbeedy@gmail.com>
2025-08-25 15:01:02 +0000

    add vantage-jupyterhub venv

--- public-scripts/deploy-democluster.sh
+++ public-scripts/deploy-democluster.sh
@@ -164,7 +164,7 @@ EOF
   cat /tmp/cloud-init.yaml | multipass launch -c$(nproc) \
   -m4G \
   -d8G \
-  --mount=$HOME/democluster:/srv/vantage-nfs \
+  --mount=$HOME/democluster:/nfs \
   -n democluster-`echo "$CLIENT_ID" | sed 's/-[0-9a-f]\{8\}-[0-9a-f]\{4\}-4[0-9a-f]\{3\}-[89abAB][0-9a-f]\{3\}-[0-9a-f]\{12\}//'` \
   $IMAGE_ORIGIN \
   --cloud-init -
